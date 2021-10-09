b7d816cdb39b5e6fed2d3fbfd206d612a1e9cafb
racka98 <bmkilaha@gmail.com>
2021-10-09 14:40:31 +0300

    theme and splash screen setup

--- app/src/main/java/com/example/composetemplate/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/composetemplate/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.composetemplate.ui.components.Greeting
 import com.example.composetemplate.ui.theme.ComposeAndroidTemplateTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Enable support for Splash Screen API for
+        // proper Android 12+ support
+        installSplashScreen()
+
         setContent {
             ComposeAndroidTemplateTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/com/example/composetemplate/ui/theme/Color.kt
+++ app/src/main/java/com/example/composetemplate/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.example.composetemplate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val PrimaryColor = Color(0xffea80fc)
 val PrimaryLightColor = Color(0xffffb2ff)
 val PrimaryDarkColor = Color(0xffb64fc8)
